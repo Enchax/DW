b952e10c92bdad51da74547d85cffd8749dce70f
Enchax <mr.aungnyeinchan.aa@gmail.com>
2020-05-08 12:13:16 +0630

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,314 @@
+/**
+* Template Name: TheEvent - v2.0.0
+* Template URL: https://bootstrapmade.com/theevent-conference-event-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+!(function($) {
+  "use strict";
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+  // Header fixed on scroll
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+
+  // Real view height for mobile devices
+  if (window.matchMedia("(max-width: 767px)").matches) {
+    $('#intro').css({
+      height: $(window).height()
+    });
+  }
+
+  // Initiate the wowjs animation library
+  new WOW().init();
+
+  // Initialize Venobox
+  $('.venobox').venobox({
+    bgcolor: '',
+    overlayColor: 'rgba(6, 12, 34, 0.85)',
+    closeBackground: '',
+    closeColor: '#fff'
+  });
+
+  // Initiate superfish on nav menu
+  $('.nav-menu').superfish({
+    animation: {
+      opacity: 'show'
+    },
+    speed: 400
+  });
+
+  // Mobile Navigation
+  if ($('#nav-menu-container').length) {
+    var $mobile_nav = $('#nav-menu-container').clone().prop({
+      id: 'mobile-nav'
+    });
+    $mobile_nav.find('> ul').attr({
+      'class': '',
+      'id': ''
+    });
+    $('body').append($mobile_nav);
+    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
+    $('body').append('<div id="mobile-body-overly"></div>');
+    $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
+
+    $(document).on('click', '.menu-has-children i', function(e) {
+      $(this).next().toggleClass('menu-item-active');
+      $(this).nextAll('ul').eq(0).slideToggle();
+      $(this).toggleClass("fa-chevron-up fa-chevron-down");
+    });
+
+    $(document).on('click', '#mobile-nav-toggle', function(e) {
+      $('body').toggleClass('mobile-nav-active');
+      $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+      $('#mobile-body-overly').toggle();
+    });
+
+    $(document).click(function(e) {
+      var container = $("#mobile-nav, #mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').fadeOut();
+        }
+      }
+    });
+  } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
+    $("#mobile-nav, #mobile-nav-toggle").hide();
+  }
+
+  // Smooth scroll for the menu and links with .scrollto classes
+  $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        var top_space = 0;
+
+        if ($('#header').length) {
+          top_space = $('#header').outerHeight();
+
+          if (!$('#header').hasClass('header-fixed')) {
+            top_space = top_space - 20;
+          }
+        }
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - top_space
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu').length) {
+          $('.nav-menu .menu-active').removeClass('menu-active');
+          $(this).closest('li').addClass('menu-active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').fadeOut();
+        }
+        return false;
+      }
+    }
+  });
+
+  // Gallery carousel (uses the Owl Carousel library)
+  $(".gallery-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    center: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 3
+      },
+      992: {
+        items: 4
+      },
+      1200: {
+        items: 5
+      }
+    }
+  });
+  jQuery(document).ready(function($){
+    var $form_modal = $('.user-modal'),
+      $form_login = $form_modal.find('#login'),
+      $form_signup = $form_modal.find('#signup'),
+      $form_forgot_password = $form_modal.find('#reset-password'),
+      $form_modal_tab = $('.switcher'),
+      $tab_login = $form_modal_tab.children('li').eq(0).children('a'),
+      $tab_signup = $form_modal_tab.children('li').eq(1).children('a'),
+      $forgot_password_link = $form_login.find('.form-bottom-message a'),
+      $back_to_login_link = $form_forgot_password.find('.form-bottom-message a'),
+      $main_nav = $('.main-nav');
+  
+    //open modal
+    $main_nav.on('click', function(event){
+  
+      if( $(event.target).is($main_nav) ) {
+        // on mobile open the submenu
+        $(this).children('ul').toggleClass('is-visible');
+      } else {
+        // on mobile close submenu
+        $main_nav.children('ul').removeClass('is-visible');
+        //show modal layer
+        $form_modal.addClass('is-visible'); 
+        //show the selected form
+        ( $(event.target).is('.signup') ) ? signup_selected() : login_selected();
+      }
+  
+    });
+  
+    //close modal
+    $('.user-modal').on('click', function(event){
+      if( $(event.target).is($form_modal) || $(event.target).is('.close-form') ) {
+        $form_modal.removeClass('is-visible');
+      } 
+    });
+    //close modal when clicking the esc keyboard button
+    $(document).keyup(function(event){
+        if(event.which=='27'){
+          $form_modal.removeClass('is-visible');
+        }
+      });
+  
+    //switch from a tab to another
+    $form_modal_tab.on('click', function(event) {
+      event.preventDefault();
+      ( $(event.target).is( $tab_login ) ) ? login_selected() : signup_selected();
+    });
+  
+    //hide or show password
+    $('.hide-password').on('click', function(){
+      var $this= $(this),
+        $password_field = $this.prev('input');
+      
+      ( 'password' == $password_field.attr('type') ) ? $password_field.attr('type', 'text') : $password_field.attr('type', 'password');
+      ( 'Show' == $this.text() ) ? $this.text('Hide') : $this.text('Show');
+      //focus and move cursor to the end of input field
+      $password_field.putCursorAtEnd();
+    });
+  
+    //show forgot-password form 
+    $forgot_password_link.on('click', function(event){
+      event.preventDefault();
+      forgot_password_selected();
+    });
+  
+    //back to login from the forgot-password form
+    $back_to_login_link.on('click', function(event){
+      event.preventDefault();
+      login_selected();
+    });
+  
+    function login_selected(){
+      $form_login.addClass('is-selected');
+      $form_signup.removeClass('is-selected');
+      $form_forgot_password.removeClass('is-selected');
+      $tab_login.addClass('selected');
+      $tab_signup.removeClass('selected');
+    }
+  
+    function signup_selected(){
+      $form_login.removeClass('is-selected');
+      $form_signup.addClass('is-selected');
+      $form_forgot_password.removeClass('is-selected');
+      $tab_login.removeClass('selected');
+      $tab_signup.addClass('selected');
+    }
+  
+    function forgot_password_selected(){
+      $form_login.removeClass('is-selected');
+      $form_signup.removeClass('is-selected');
+      $form_forgot_password.addClass('is-selected');
+    }
+  
+    //REMOVE THIS - it's just to show error messages 
+    $form_login.find('input[type="submit"]').on('click', function(event){
+      event.preventDefault();
+      $form_login.find('input[type="email"]').toggleClass('has-error').next('span').toggleClass('is-visible');
+    });
+    $form_signup.find('input[type="submit"]').on('click', function(event){
+      event.preventDefault();
+      $form_signup.find('input[type="email"]').toggleClass('has-error').next('span').toggleClass('is-visible');
+    });
+  
+  
+    //IE9 placeholder fallback
+    //credits http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
+    if(!Modernizr.input.placeholder){
+      $('[placeholder]').focus(function() {
+        var input = $(this);
+        if (input.val() == input.attr('placeholder')) {
+          input.val('');
+          }
+      }).blur(function() {
+        var input = $(this);
+          if (input.val() == '' || input.val() == input.attr('placeholder')) {
+          input.val(input.attr('placeholder'));
+          }
+      }).blur();
+      $('[placeholder]').parents('form').submit(function() {
+          $(this).find('[placeholder]').each(function() {
+          var input = $(this);
+          if (input.val() == input.attr('placeholder')) {
+            input.val('');
+          }
+          })
+      });
+    }
+  
+  });
+  
+  
+  //credits https://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
+  jQuery.fn.putCursorAtEnd = function() {
+    return this.each(function() {
+        // If this function exists...
+        if (this.setSelectionRange) {
+            // ... then use it (Doesn't work in IE)
+            // Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
+            var len = $(this).val().length * 2;
+            this.setSelectionRange(len, len);
+        } else {
+          // ... otherwise replace the contents with itself
+          // (Doesn't work in Google Chrome)
+            $(this).val($(this).val());
+        }
+    });
+  };
+  // Buy tickets select the ticket type on click
+  $('#buy-ticket-modal').on('show.bs.modal', function(event) {
+    var button = $(event.relatedTarget);
+    var ticketType = button.data('ticket-type');
+    var modal = $(this);
+    modal.find('#ticket-type').val(ticketType);
+  })
+
+})(jQuery);
